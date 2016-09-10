153236329df2e91bb847ca1a28b80c6bd37b56d8
natandanovich <natan.danovich@outlook.com>
2016-09-10 09:41:14 +0300

    Upgrade express version and add startup app.JS  package,json Learn to use simple routing Learn to start the server with port 3000

--- nodeJsProject/nodeproject/app.js
+++ nodeJsProject/nodeproject/app.js
@@ -1,2 +1,19 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.get('/', function (req,res) {
+    res.send('Hello World');
+});
+
+app.get('/routing', function (req,res) {
+    res.send('Hello Rounting');
+});
+
+app.listen(port, function (err) {
+    if (err.message != '') {
+      console.log(err.message)
+    }
+    console.log('The server is running on port: ' + port);
+});
