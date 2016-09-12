428b6cc858f49d09335fff7dd4598aee720f6dbe
natand <natand@razorgrip.com>
2016-09-12 07:55:09 +0300

    Re-organize the folder structure
    Add bower
    Change the resource path in index.html to bower

--- nodeJsProject/nodeproject/app.js
+++ nodeJsProject/nodeproject/app.js
@@ -3,6 +3,10 @@ var app = express();
 
 var port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
 app.get('/', function (req,res) {
     res.send('Hello World');
 });
