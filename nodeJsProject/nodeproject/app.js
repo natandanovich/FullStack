0442273c8d3dd6f6968d659179d1690d56424fe6
natand <natan.danovich@outlook.com>
2016-09-12 16:50:21 +0300

    Learn to create more sophisticated implementation of EJS in the HTML
    and give more simple and beautiful organized code

--- nodeJsProject/nodeproject/app.js
+++ nodeJsProject/nodeproject/app.js
@@ -6,22 +6,25 @@ var port = 3000;
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-app.set('views','./src/views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req,res) {
+app.get('/', function (req, res) {
     //res.send('Hello World');
-    res.render('index',{list: ['first','second','third']});
+    res.render('index', {
+        list: ['first', 'second', 'third'],
+        nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+    });
 });
 
 
-app.get('/routing', function (req,res) {
+app.get('/routing', function (req, res) {
     res.send('Hello Rounting');
 });
 
 app.listen(port, function (err) {
     if (err) {
-      console.log(err)
+        console.log(err)
     }
     console.log('The server is running on port: ' + port);
 });
