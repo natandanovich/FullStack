b95680df5581f285bfbc14c4561d295f51cf5cbb
natand <natan.danovich@outlook.com>
2016-09-12 16:14:23 +0300

    Add ejs (EJS is a client-side templating language, EJS combines data and a template to produce HTML. )
    Command: npm install --save ejs
    
    Rename the index.html to index.ejs
    Add small example of how to use EJS Template inside html file

--- nodeJsProject/nodeproject/app.js
+++ nodeJsProject/nodeproject/app.js
@@ -4,11 +4,14 @@ var app = express();
 var port = 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views','./src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req,res) {
-    res.send('Hello World');
+    //res.send('Hello World');
+    res.render('index',{list: ['first','second','third']});
 });
 
 
