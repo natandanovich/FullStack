34f94df7dd3ba6f488a3f5c265bafc80fdadc048
natand <natan.danovich@outlook.com>
2016-09-21 15:20:44 +0300

    Upgrade the project with new packages

--- StartingWithAngular2/gulpfile.js
+++ StartingWithAngular2/gulpfile.js
@@ -6,49 +6,50 @@ var tsProject = tsc.createProject('tsconfig.json');
 var config = require('./gulp.config')();
 var browserSync = require('browser-sync');
 var superstatic = require('superstatic');
+var port = 3000;
 
-gulp.task('ts-lint', function() {
-	return gulp.src(config.allTs)
-		.pipe(tslint())
-		.pipe(tslint.report('prose', {
-			emitError: false
-	    }))
-});
-	
-gulp.task('compile-ts', function(){
-	var sourceTsFiles = [
-		config.allTs,
-		config.typings
-	];
-		
-	var tsResult = gulp
-		.src(sourceTsFiles)
-		.pipe(sourcemaps.init())
-		.pipe(tsc(tsProject));
-			
-		return tsResult.js
-		    .pipe(sourcemaps.write('.'))
-			.pipe(gulp.dest(config.tsOutputPath));
-			
+gulp.task('ts-lint', function () {
+    return gulp.src(config.allTs)
+        .pipe(tslint())
+        .pipe(tslint.report('prose', {
+            emitError: false
+        }))
 });
 
-gulp.task('serve', ['ts-lint', 'compile-ts'], function(){
-	//watch for changes... if a change: lint, recompile
-	gulp.watch([config.allTs], ['ts-lint', 'compile-ts']);
-		
-	browserSync({
-		port: process.env.PORT,
-		file: ['index.html', '**/*.js'],
-		injectChanges: true,
-		logFileChanges: false,
-		logLevel: 'silent',
-		notify: true,
-		reloadDelay: 0,
-		server: {
-			baseDir: './app',
-			middleware: superstatic({debug: false})
-		}
-	});
+gulp.task('compile-ts', function () {
+    var sourceTsFiles = [
+        config.allTs,
+        config.typings
+    ];
+
+    var tsResult = gulp
+        .src(sourceTsFiles)
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsProject));
+
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.tsOutputPath));
+
+});
+
+gulp.task('serve', ['ts-lint', 'compile-ts'], function () {
+    //watch for changes... if a change: lint, recompile
+    gulp.watch([config.allTs], ['ts-lint', 'compile-ts']);
+
+    browserSync({
+        port: port,
+        file: ['index.html', '**/*.js'],
+        injectChanges: true,
+        logFileChanges: false,
+        logLevel: 'silent',
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: './app',
+            middleware: superstatic({debug: false})
+        }
+    });
 });
 
 gulp.task('default', ['serve']);
